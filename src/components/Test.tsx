806704c37fb46a83c8516c0b52fb7088067cb3b3
rywww <rkupeseva@gmail.com>
2023-12-03 20:40:39 +0600

    styles

---
+++ src/components/Test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import { motion } from "framer-motion";
+
+const Test = () => {
+  return (
+    <div>
+      <div className="heroes">
+        {/* <motion.div className="box" animate={{ opacity: 0 }}></motion.div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Test;
---
+++ src/pages/NewPage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const NewPage: React.FC = () => {
+  return (
+    <div className="video-section">
+      {/* <video className="background-video" autoPlay loop muted>
+        <source
+          src="//akmweb.youngjoygame.com/web/admin/other/22c82130ce3d7b9e6a5146611c5fc1ef.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video> */}
+      <div className="new-page-container">
+        <h1>New Page</h1>
+        <p>aklsdfj;aklsdfj</p>
+        <p></p>
+        <div className="scrollable-content">{}</div>
+      </div>
+    </div>
+  );
+};
+
+export default NewPage;
